4604dfec28a5d800da8a7339cdad522684bf6227
Nathan Evans <github@talkswithnumbers.com>
2015-03-26 17:48:55 -0700

    added geoprocessing module with spatial join

---
+++ js/app/geoprocessing.js
@@ -0,0 +1,130 @@
+"use strict";
+/**
+ * Geoprocessing utilities for doing spatial analysis within the Google Maps API.
+ * All they really provide is hit testing, so we'll expand that to include any
+ * standard geoprocessing algorithms that we need.
+ * See https://developers.google.com/maps/documentation/javascript/3.exp/reference#spherical
+ * and https://developers.google.com/maps/documentation/javascript/3.exp/reference#poly
+ */
+define({
+
+    /**
+     * Calculates a bounding box for a google.maps.Polygon
+     * TODO: return a LtLngBounds to be directly google api compatible
+     * @param polygon
+     */
+    bounds: function (polygon) {
+        var minx = 180, maxx = -180, miny = 90, maxy = -90;
+        polygon.getPath().forEach(function (ltlng) {
+            var lng = ltlng.lng(),
+                lat = ltlng.lat();
+            minx = Math.min(minx, lng);
+            maxx = Math.max(maxx, lng);
+            miny = Math.min(miny, lat);
+            maxy = Math.max(maxy, lat);
+        });
+        return {
+            minx: minx,
+            maxx: maxx,
+            miny: miny,
+            maxy: maxy
+        };
+    },
+
+    /**
+     * Joins a set of points into a set of polygons.
+     * Note that for now it simply sums the point count and adds it as a property to the polygon Feature.
+     * @param {Feature[]} polygons - array of Features that represent the polygons to check points against.
+     * @param {Feature[]} points - array of Features that represent the points to bin into polygons.
+     * @param {String} property - property name to store the point count value
+     */
+    spatialJoinWithin: function (polygons, points, property) {
+
+        var start = Date.now(),
+            getBounds = this.bounds,
+            polyCount = 0,
+            comparisonCount = 0;
+
+        //sort ascending x, so we can short-circuit hit test loops
+        //this lets us only do an actual hit test on points within a longitude "band",
+        //which saves quite a bit of time
+        points.sort(function (p1, p2) {
+            return p1.getGeometry().get().lng() - p2.getGeometry().get().lng();
+        });
+
+        //finds first index greater than lng
+        function findMinIndex(points, lng) {
+            //TODO: replace this with a binary search
+            for (var i = 0; i < points.length; i += 1) {
+                if (points[i].getGeometry().get().lng() >= lng) {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        polygons.forEach(function (polygon) {
+
+            var count = 0,
+                poly,
+                bounds,
+                index,
+                point,
+                i;
+
+            try {
+
+                //need to convert data polygons to base polygons for containsLocation
+                poly = new google.maps.Polygon({
+                        paths: polygon.getGeometry().getArray().map(function (ring) {
+                            return ring.getArray();
+                        })
+                    });
+
+                bounds = getBounds(poly);
+
+                index = findMinIndex(points, bounds.minx);
+
+                for (i = index; i < points.length; i += 1) {
+
+                    point = points[i];
+                    comparisonCount += 1;
+
+                    try {
+                        var pointLtLng = point.getGeometry().get(),
+                            lng = pointLtLng.lng(),
+                            contained;
+
+                        contained = google.maps.geometry.poly.containsLocation(pointLtLng, poly);
+
+                        if (contained) {
+                            count += 1;
+                        } else if (lng > bounds.maxx) {
+                            break;
+                        }
+
+                    } catch (e) {
+                        //ignore bad points
+                        console.log('Error with point ' + point.getId());
+                    }
+
+                }
+
+                polyCount += 1;
+
+            } catch (e) {
+                //some of the census data may have unsupported geometry for this simple analysis (e.g., donuts)
+                console.log('Error joining with polygon ' + polygon.getId());
+            } finally {
+                polygon.setProperty(property, count);
+            }
+
+        });
+
+        console.log('successful polygons ' + polyCount);
+        console.log('comparisons: ' + comparisonCount);
+        console.log('spatial join: ' + (Date.now() - start) + 'ms');
+
+    }
+
+});
---
+++ js/app/geoprocessing.test.js
@@ -0,0 +1,118 @@
+"use strict";
+
+requirejs.config({
+    config: {
+        'js/app/map': {
+            map: {
+                center: { lat: 42.3601, lng: -71.0589 },
+                zoom: 12
+            },
+            element: '#map-canvas'
+        }
+    }
+});
+
+//setup a spot for the map to be added within the karma runner
+//WARNING: this is pretty kludgy
+var canvas = document.createElement('div');
+canvas.id = 'map-canvas';
+document.body.appendChild(canvas);
+
+define([
+    'jquery',
+    './map',
+    './geoprocessing'
+], function (
+    jQuery,
+    map,
+    geoprocessing
+) {
+
+    describe('geoprocessing.js', function () {
+
+        this.timeout(10000);
+
+        var polygons,
+            points;
+
+        //load up a map and test data sets
+        //obviously this isn't really 'unit' testing unless we mock out the google APIs
+        beforeEach(function (done) {
+
+            var counter = 0;
+            function check() {
+                if (counter === 2) {
+                    done();
+                }
+            }
+
+            jQuery.ajax(
+                'http://labs.atsid.com/hubhacks2/data/tracts-small.geojson',
+                //'http://labs.atsid.com/hubhacks2/data/cb_2013_25_tract_500k.geojson',
+                {
+                    success: function (data) {
+                        polygons = map.data.addGeoJson(data);
+                        counter += 1;
+                        check();
+                    }
+                }
+            );
+
+            jQuery.ajax(
+                'http://labs.atsid.com/hubhacks2/data/food-small.geojson',
+                {
+                    success: function (data) {
+                        points = map.data.addGeoJson(data);
+                        counter += 1;
+                        check();
+                    }
+                }
+            );
+
+        });
+
+        it('spatial join WITHIN', function () {
+
+            console.log('polygon count ' + polygons.length);
+            console.log('point count ' + points.length);
+
+            //this subset was hand-counted in arcgis to validate the spatial join
+            var assertions = {
+                '1008': 0,
+                '1002': 0,
+                '1009': 4,
+                '1011.01': 3,
+                '1010.02': 7,
+                '1003': 0,
+                '1011.02': 3,
+                '1404': 1,
+                '4163': 0,
+                '4162': 0,
+                '1010.01': 4,
+                '1403': 0,
+                '1001': 0
+            },
+                assertionCount = 0;
+
+
+            
+            geoprocessing.spatialJoinWithin(polygons, points, 'food_count');
+
+            polygons.forEach(function (polygon) {
+
+                var name = polygon.getProperty('name'),
+                    count = polygon.getProperty('food_count');
+
+                if (typeof assertions[name] !== 'undefined') { //watch for falsy 0
+                    assertionCount += 1;
+                    expect(count).to.equal(assertions[name]);
+                }
+
+            });
+
+            expect(assertionCount).to.equal(13); //just make sure we got them all
+        });
+
+    });
+
+});
