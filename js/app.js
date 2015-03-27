344ae8a88ef51561ca22d6390c064beb02319632
Joe Jaworski <joe.jaworski@atsid.com>
2015-03-26 18:33:20 -0700

    Adding an About box with the entire Census Bureau message.

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ requirejs.config({
     'baseUrl': 'js/lib',
     'paths': {
         'app': '../app',
-        'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min'
+        'jquery': 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min',
+        'jqueryui': 'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min'
     },
     'config': {
         'app/map': {
